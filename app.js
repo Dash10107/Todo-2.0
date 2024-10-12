6370b23079d4aeaa0df6b2cc0233d99fe61c1b38
Daksh C Jain <97282628+Dash10107@users.noreply.github.com>
2024-10-12 18:50:48 +0530

    Update app.js removed db link

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://Dash10107:test123@cluster0.dxl9zuf.mongodb.net/todolistDB");
+mongoose.connect(process.env.MONGO_URL);
 
 const itemsSchema = {
   name:String,
